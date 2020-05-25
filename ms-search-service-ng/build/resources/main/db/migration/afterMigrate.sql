1417b26cb25358147abaedd653396f0c44f55dd0
Alejandro Prado <aprado@mahisoft.com>
2020-05-25 15:06:18 -0300

    a

---
+++ ms-search-service-ng/build/resources/main/db/migration/afterMigrate.sql
@@ -0,0 +1,9 @@
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('Date - Time', "message_date", 1, 1);
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('Network', "network", 1, 1);
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('Channel', "channel", 1, 1);
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('Message', "message", 1, 1);
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('User Name', "user_name", 1, 1);
+
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('IP', "ipaddress", 0, 1);
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('Country', "LocateCountry", 0, 1);
+INSERT IGNORE INTO index_column (`column_name`, `column_code`, `is_filter_column`, `is_display_column`) VALUES ('City', "LocateCity", 0, 1);
